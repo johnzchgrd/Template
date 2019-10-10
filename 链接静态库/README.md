f7f71bb9c35629c9377ebe53fa7455f2d8c84e85
Johnzchgrd <hzyhzch@qq.com>
2019-10-10 23:41:57 +0800

    初步实现静态库链接

---
+++ 链接静态库/README.md
@@ -0,0 +1,1 @@
+### ref: https://github.com/mbcrawfo/GenericMakefile
---
+++ 链接静态库/lib/static/test.c
@@ -0,0 +1,13 @@
+#include <local/test.h>
+int first_function(int x)
+{
+    return x + 1;
+}
+int second_function(int x)
+{
+    return x + 2;
+}
+int third_function(int x)
+{
+    return x + 3;
+}
